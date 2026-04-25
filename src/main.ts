a075b19731f188d80f51947b1c34a211ac04ae84
Suhaimee <suhaimee.kj@gmail.com>
2026-04-25 20:21:17 +0700

    fix: use CDN for swagger assets on Vercel

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,11 @@ async function bootstrap() {
     swaggerOptions: {
       persistAuthorization: true,
     },
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
   });
   
   const port = process.env.PORT ?? 8080;
